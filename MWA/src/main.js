5b306db9cba4b11281387e4db257045914b691b3
omarIbrahim2 <rady1753@gmail.com>
2023-12-01 17:57:13 +0200

    sidenav component

--- MWA/src/main.js
+++ MWA/src/main.js
@@ -3,6 +3,14 @@ import './assets/admin/css/tempusdominus-bootstrap-4.css'
 import './assets/admin/css/adminlte.css'
 import './assets/admin/css/rtl/bootstrap.css'
 import './assets/admin/css/custom.css'
+import './assets/admin/js/jquery.js'
+import './assets/admin/js/jquery-ui.js'
+$.widget.bridge('uibutton', $.ui.button)
+import './assets/admin/js/rtl/bootstrap.js'
+import './assets/admin/js/bootstrap.bundle.js'
+import './assets/admin/js/adminlte.js'
+import './assets/admin/js/dashboard.js'
+import './assets/admin/js/demo.js'
 
 
 import { createApp } from 'vue'
--- MWA/src/router/index.js
+++ MWA/src/router/index.js
@@ -1,14 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Dashboard from '../components/Dashboard.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/dashboard',
-      name: 'dashboard',
-      component: Dashboard
-    },
+   
     
   ]
 })
