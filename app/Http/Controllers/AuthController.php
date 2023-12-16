0809f7336f574ee7e45cbeaf9c850ec0d4e44846
Mohamed Gharby <Mohamed.Gharby@geidea.net>
2023-12-16 14:19:00 +0200

    auth first step

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,21 +21,27 @@ class AuthController extends Controller
         $this->fileService = $fileService ;
     }
 
-    // public function register(AuthRegisterReq $request , AuthRegisterMerchantReq $merReq , AuthRegisterCustomerReq $cusReq){
-    //     $data = $request->validated();
-    //     $data["password"] = bcrypt($data["password"]);
-    //     $user = User::create($data);
-    //     $data["token"] = $user->createToken("UserToken")->plainTextToken;
-    //     if($user->role_id == Role::where("name" , "Customer")->first->id ){
-    //         $customerData = $cusReq->validated();
-    //         Customer::create($customerData);
-    //         return response()->json($data, 201);
-    //     }elseif ($user->role_id == Role::where("name" , "Merchant")->first->id ) {
-    //         $merchantData = $merReq->validated(); 
-    //         $merchantData['img'] = $this->handleUpload($request , $this->fileService , null , 'Merchants');
-    //         Merchant::create($merchantData);
-    //         return response()->json($data, 201);
-    //     };
+    public function register(AuthRegisterReq $request , AuthRegisterMerchantReq $merReq , AuthRegisterCustomerReq $cusReq){
+        $data = $request->validated();
+        $data["password"] = bcrypt($data["password"]);
+        $user = User::create($data);
+        $data["token"] = $user->createToken("UserToken")->plainTextToken;
+        $customerRole = Role::where("name" , "Customer")->first() ;
+        $customerRoleId = $customerRole->id ;
+        $merchantRole =Role::where("name" , "Merchant")->first() ;
+        $merchantRoleId = $merchantRole->id ;
+        if($user->role_id == $customerRoleId ){
+            $customerData = $cusReq->validated();
+            $customerData['user_id'] = $user->id ;
+            $user->customer()->create($customerData);
+            return response()->json($data, 201);
+        }elseif ($user->role_id ==  $merchantRoleId) {
+            $merchantData = $merReq->validated(); 
+            $merchantData['user_id'] = $user->id ;
+            $merchantData['img'] = $this->handleUpload($merReq , $this->fileService , null , 'Merchants');
+            $user->merchant()->create($merchantData);
+            return response()->json($data, 201);
+        };
 
-    // }
+    }
 }
